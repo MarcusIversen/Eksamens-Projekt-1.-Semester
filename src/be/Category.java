4430d0b55db102bb78f51c276e8121899ccd3746
Andylaa10 <andylaa100@gmail.com>
2022-01-04 12:48:25 +0100

    getters and setters

--- src/be/Category.java
+++ src/be/Category.java
@@ -1,4 +1,38 @@
 package be;
 
 public class Category {
+
+    private int id;
+    private String name;
+    private String numberOfMovies;
+
+    public Category(int id, String name, String numberOfMovies){
+        this.id = id;
+        this.name = name;
+        this.numberOfMovies = numberOfMovies;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumberOfMovies() {
+        return numberOfMovies;
+    }
+
+    public void setNumberOfMovies(String numberOfMovies) {
+        this.numberOfMovies = numberOfMovies;
+    }
 }
--- src/be/Movie.java
+++ src/be/Movie.java
@@ -1,2 +1,58 @@
-package be;public class Movie {
+package be;
+
+public class Movie {
+
+    private int id;
+    private String name;
+    private double rating;
+    private String fileLink;
+    private String lastView;
+
+    public Movie(int id, String name, double rating, String fileLink, String lastView) {
+        this.id = id;
+        this.name = name;
+        this.rating = rating;
+        this.fileLink = fileLink;
+        this.lastView = lastView;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public String getFileLink() {
+        return fileLink;
+    }
+
+    public void setFileLink(String fileLink) {
+        this.fileLink = fileLink;
+    }
+
+    public String getLastView() {
+        return lastView;
+    }
+
+    public void setLastView(String lastView) {
+        this.lastView = lastView;
+    }
 }
