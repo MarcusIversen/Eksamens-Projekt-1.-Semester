83359c33f2622697a0a70120a6c00113abb93d77
MarcusIversen <91528331+MarcusIversen@users.noreply.github.com>
2022-01-04 11:44:35 +0100

    DatabaseConnector

--- src/dal/DatabaseConnector.java
+++ src/dal/DatabaseConnector.java
@@ -1,4 +1,41 @@
 package dal;
 
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class DatabaseConnector {
+
+    private SQLServerDataSource dataSource;
+
+    /**
+     * Database connector constructoren, her skriver vi alle vores login data til databasen.
+     */
+    public DatabaseConnector() {
+        dataSource = new SQLServerDataSource();
+        dataSource.setServerName("10.176.111.31");
+        dataSource.setDatabaseName("PrivateMovieCollection2022");
+        dataSource.setUser("CSe21A_5");
+        dataSource.setPassword("CSe21A_5");
+        dataSource.setPortNumber(1433);
+    }
+
+    /**
+     * Her bruges vores database login data til at connecte, ved brug af getConnection();
+     * @return
+     * @throws SQLServerException
+     */
+    public Connection getConnection() throws SQLServerException {
+        return dataSource.getConnection();
+    }
+
+    public static void main(String[] args) throws SQLException {
+        DatabaseConnector databaseConnector = new DatabaseConnector();
+
+        try(Connection connection = databaseConnector.getConnection()){
+            System.out.println("is it open?  :  " + !connection.isClosed());
+        }
+    }
 }
