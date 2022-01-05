d78b63dce5ceb9273aa8db022d4648ce106e980f
MarcusIversen <91528331+MarcusIversen@users.noreply.github.com>
2022-01-05 13:46:31 +0100

    CSS på GUI

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/gui/view/MainMenu.fxml"));
         primaryStage.setTitle("My Movie Collection");
-        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 }
