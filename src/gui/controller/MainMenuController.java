7c80ca4b1c014d873214f91b331ade9403bf7238
Ferniss <mathiasf2000@hotmail.com>
2022-01-05 09:02:21 +0100

    new button open new window (new window is not modified yet))

--- src/gui/controller/MainMenuController.java
+++ src/gui/controller/MainMenuController.java
@@ -1,8 +1,23 @@
 package gui.controller;
 
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainMenuController {
+    private Stage stage = new Stage();
+
+    public void createNewMovieButton() throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/gui/view/addMovie.fxml"));
+        stage.setTitle("New Song");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
 
     public void closeTheAppButton() {
         Platform.exit();
