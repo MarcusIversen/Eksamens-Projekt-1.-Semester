071847052e00aa15296e94d42e0d07506968f2a0
MarcusIversen <91528331+MarcusIversen@users.noreply.github.com>
2022-01-06 12:35:36 +0100

    Duration

--- src/gui/controller/OpeningMenuController.java
+++ src/gui/controller/OpeningMenuController.java
@@ -16,7 +16,7 @@ public class OpeningMenuController {
 
     public void goMainMenu() throws IOException{
         Stage switcher = (Stage) btnGoToMainMenu.getScene().getWindow();
-        Parent parent = FXMLLoader.load(getClass().getResource("../view/MainMenu.fxml"));
+        Parent parent = FXMLLoader.load(getClass().getResource("/gui/view/MainMenu.fxml"));
         Scene scene = new Scene(parent);
         switcher.setScene(scene);
     }
