4605509a31e84c16fa06fa33844d495c55f79ced
Ferniss <mathiasf2000@hotmail.com>
2022-01-10 09:52:53 +0100

    Comments in Category

--- src/be/Category.java
+++ src/be/Category.java
@@ -10,7 +10,7 @@ public class Category {
     private List<Movie> movies = new ArrayList<>();
 
     /**
-     *
+     * Constructor with id, name, and movies
      * @param id
      * @param name
      * @param movies
@@ -21,7 +21,7 @@ public class Category {
     }
 
     /**
-     *
+     * Constructor with id and name
      * @param id
      * @param name
      */
@@ -31,31 +31,56 @@ public class Category {
     }
 
     /**
-     *
+     * Gets the number moviesCount
      * @return
      */
     public int getMovieCount() {
         return movies.size();
     }
 
+    /**
+     * Gets the value of id
+     * @return
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Sets the value of id
+     * @return
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+
+    /**
+     * Gets value of name
+     * @return
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets value of name
+     * @return
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets value of movies
+     * @return
+     */
     public List<Movie> getMovies() { return movies; }
 
+    /**
+     * Sets value of movies
+     * @return
+     */
     public void setMovies(List<Movie> movies) {
         this.movies = movies;
     }
