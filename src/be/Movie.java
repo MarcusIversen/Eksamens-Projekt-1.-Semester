23a24f8586157a161c0904b7c21cf58832c3a4b4
Andylaa10 <andylaa100@gmail.com>
2022-01-10 10:00:24 +0100

    Comments

--- src/be/Movie.java
+++ src/be/Movie.java
@@ -9,12 +9,25 @@ public class Movie {
     private String lastView;
     private int duration;
 
+    /**
+     * Constructor with id, name and rating
+     * @param id
+     * @param name
+     * @param rating
+     */
     public Movie(int id, String name, String rating) {
         this.id = id;
         this.name = name;
         this.rating = rating;
     }
 
+    /**
+     * Constructor with id, name, rating and fileLink
+     * @param id
+     * @param name
+     * @param rating
+     * @param fileLink
+     */
     public Movie(int id, String name, String rating, String fileLink) {
         this.id = id;
         this.name = name;
@@ -22,6 +35,14 @@ public class Movie {
         this.fileLink = fileLink;
     }
 
+    /**
+     * Constructor with id, name, rating, fileLink and duration
+     * @param id
+     * @param name
+     * @param rating
+     * @param fileLink
+     * @param duration
+     */
     public Movie(int id, String name, String rating, String fileLink, int duration) {
         this.id = id;
         this.name = name;
@@ -30,6 +51,14 @@ public class Movie {
         this.duration = duration;
     }
 
+    /**
+     * Constructor with id, name, rating, fileLink and lastView
+     * @param id
+     * @param name
+     * @param rating
+     * @param fileLink
+     * @param lastView
+     */
     public Movie(int id, String name, String rating, String fileLink, String lastView) {
         this.id = id;
         this.name = name;
@@ -38,6 +67,15 @@ public class Movie {
         this.lastView = lastView;
     }
 
+    /**
+     * Constructor with id, name, rating, fileLink, lastView and duration
+     * @param id
+     * @param name
+     * @param rating
+     * @param fileLink
+     * @param lastView
+     * @param duration
+     */
     public Movie(int id, String name, String rating, String fileLink, String lastView, int duration) {
         this.id = id;
         this.name = name;
@@ -47,57 +85,103 @@ public class Movie {
         this.duration = duration;
     }
 
+    /**
+     * Gets value of id
+     * @return id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Sets value of id
+     * @param id
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Gets value of name
+     * @return name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets value of name
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets value of rating
+     * @return rating
+     */
     public String getRating() {
         return rating;
     }
 
+    /**
+     * Sets value of rating
+     * @param rating
+     */
     public void setRating(String rating) {
         this.rating = rating;
     }
 
-    public void setDuration(int duration) { this.duration = duration; }
-
     /**
-     *
+     * Gets the value of duration
      * @return
      */
     public String getDuration() {
-        int minuts = (duration)/60;
+        int minutes = (duration)/60;
         int seconds = (duration) %60;
         if (10 > seconds){
-            return minuts + ":0" + seconds;
+            return minutes + ":0" + seconds;
         } else {
-            return minuts + ":" + seconds;
+            return minutes + ":" + seconds;
         }}
 
+    /**
+     * Sets the value of duration
+     * @param duration
+     */
+    public void setDuration(int duration){
+        this.duration = duration;
+    }
+
+    /**
+     * Gets the value of fileLink
+     * @return fileLink
+     */
     public String getFileLink() {
         return fileLink;
     }
 
+    /**
+     * Sets the value of fileLink
+     * @param fileLink
+     */
     public void setFileLink(String fileLink) {
         this.fileLink = fileLink;
     }
 
+    /**
+     * Gets the value of lastView
+     * @return
+     */
     public String getLastView() {
         return lastView;
     }
 
+    /**
+     * Sets the value of lastView
+     * @param lastView
+     */
     public void setLastView(String lastView) {
         this.lastView = lastView;
     }
